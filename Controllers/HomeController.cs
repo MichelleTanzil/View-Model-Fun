68e6f4ddfd88048135f30407ebcad8752200a9cf
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-27 12:56:08 -0800

    edited string issue to work now

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace view_models_fun.Controllers
         public IActionResult Index()
         {
             string newMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            return View((object)newMessage);
+            return View("Index", newMessage);
         }
         [HttpGet("numbers")]
         public IActionResult Numbers()
